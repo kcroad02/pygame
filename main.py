687f5ab9abb54a80988dfbc6f79b1208c4f980e3
K.C. Roadarmel <kc@rogbone.com>
2024-09-10 00:43:17 -0400

    dafluffy

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
+import sys
 import pygame
 
 # General setup
 
-# Initialize pygame, ALWAYS INCLUDE
-pygame.init()
+class Game:
+    def __init__(self):
 
-# Define the window size
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+        # Initialize pygame, ALWAYS INCLUDE
+        pygame.init()
 
-# Screen / Display surface
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        # Define the window size
+        WINDOW_WIDTH, WINDOW_HEIGHT = 640, 480
 
+        # Screen / Display surface
+        pygame.display.set_caption("Ninja Game")
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# By default it's true
-running = True
+        self.clock = pygame.time.Clock()
 
-# Make sure the window is open every frame
-while running:
-    # event loop, check for timers, UI interactions, keyboard, etc.
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # PNG is lossless, try not to use jpg
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
 
-    # draw the game
-    display_surface.fill(('red'))
-    pygame.display.update()
-    
+    def run(self):
+        # Make sure the window is open every frame
+        while True:
 
-    # pygame.display.flip() is part of the window
-    # most of the time you don't want to do it
+            # Blue sky
+            self.screen.fill((14,219,248))
 
-# Uninitialize pygame, can cause issues, make sure to include
-pygame.quit()
+            self.img_pos[1] += self.movement[1] - self.movement[0] * 5
+            self.screen.blit(self.img, self.img_pos)
+
+            # event loop, check for timers, UI interactions, keyboard, etc.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+
+            # draw the game
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
